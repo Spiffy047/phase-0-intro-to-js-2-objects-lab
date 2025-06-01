4133e924152e35f3854dd20c81b46617898621c6
Joe M Kariuki <mwanikijoe1@gmail.com>
2025-06-01 10:24:54 +0300

    employee

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+
+const employee = {
+    name:"Joe",
+    streetAddress: "Nairobi"
+};
+function updateEmployeeWithKeyAndValue(emp, key, value) {
+  
+  return { ...emp, [key]: value };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value) {
+  
+  emp[key] = value;
+  return emp; 
+}
+function deleteFromEmployeeByKey(emp, key) {
+  
+  const newEmployee = { ...emp };
+  delete newEmployee[key]; 
+  return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(emp, key) {
+  
+  delete emp[key];
+  return emp; 
+}
